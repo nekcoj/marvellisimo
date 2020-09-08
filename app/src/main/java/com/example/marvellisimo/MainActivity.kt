448a8bb23f339c02642b214779fd35c5c47eaf4f
nekcoj <55090544+nekcoj@users.noreply.github.com>
2020-09-08 10:41:04 +0200

    added layout for comicbook page and icons for appbar etc

--- app/src/main/java/com/example/marvellisimo/MainActivity.kt
+++ app/src/main/java/com/example/marvellisimo/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.marvellisimo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            var intent = Intent(this, ComicActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
