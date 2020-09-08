2ddcb586799732cab00bca029c5e1595355e7034
Omanas98 <55090516+Omanas98@users.noreply.github.com>
2020-09-08 14:42:55 +0200

    progression

--- app/src/main/java/com/example/marvellisimo/Character.kt
+++ app/src/main/java/com/example/marvellisimo/Character.kt
@@ -6,5 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 class Character: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.character_activity)
+        actionBar?.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/marvellisimo/MainActivity.kt
+++ app/src/main/java/com/example/marvellisimo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.marvellisimo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        takeMeToCharacter.setOnClickListener {
+            var intent = Intent(this, Character::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
