88a43c6f025f1866c359bdd9407c4f12190f585f
razvan22 <razviy69@gmail.com>
2020-09-09 11:00:28 +0200

    add_basic_single_character_view

---
+++ app/src/main/java/com/example/marvellisimo/SingleCharacterView.kt
@@ -0,0 +1,11 @@
+package com.example.marvellisimo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SingleCharacterView : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.single_character_view)
+    }
+}
