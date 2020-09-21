e1ababfd9a7eb4fe4a7488186e7b180b789bb1c2
nekcoj <55090544+nekcoj@users.noreply.github.com>
2020-09-21 08:12:08 +0200

    saving changes for sprintdemo

--- app/src/main/java/com/example/marvellisimo/data/RealmData.kt
+++ app/src/main/java/com/example/marvellisimo/data/RealmData.kt
@@ -56,37 +56,21 @@ class RealmData {
             realm.use { r ->
                 r?.executeTransaction { realm ->
                     val query = realm.where<Character>().findAll()
-                    var character : com.example.marvellisimo.Character? = null
-                    for (q in query) {
-                        character?.name = q.name.toString()
-                        character?.id = q.id!!
-                    }
-                    if (character != null) {
-                        Service.characterList.add(character)
+                    for (character in query) {
+                        val url: Url = Url(character?.urls?.type, character?.urls?.url)
+                        val urls = arrayOf(url)
+                        Service.characterList.add(com.example.marvellisimo.Character(
+                            ImageDTO(character.thumbnail?.path, character.thumbnail?.extension),
+                            character?.id,
+                            character?.name,
+                            character?.description,
+                            urls,
+                            character?.favorite
+                        ))
                     }
                 }
             }
         }
-//        private fun readCharactersFromRealm(){
-//            realm = Realm.getDefaultInstance()
-//            realm.use { r ->
-//                r?.executeTransaction { realm ->
-//                    val query = realm.where<Character>().findAll()
-//                    for (q in query) {
-//                        val url: Url = Url(q?.urls?.type, q?.urls?.url)
-//                        val urls = arrayOf(url)
-//                        Service.characterList.add(com.example.marvellisimo.Character(
-//                            ImageDTO(q.thumbnail?.path, q.thumbnail?.extension),
-//                            q?.id,
-//                            q?.name,
-//                            q?.description,
-//                            urls,
-//                            q?.favorite
-//                        ))
-//                    }
-//                }
-//            }
-//        }
 
 //        private fun readComicsFromRealm() {
 //            realm = Realm.getDefaultInstance()
